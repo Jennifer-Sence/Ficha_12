3a377d9191daf82e185e13c46621545c659f91ed
Jennifer-Sence <sencejenni@gmail.com>
2024-04-17 20:43:59 +0100

    Initial commit

---
+++ src/MeioAmbiente.java
@@ -0,0 +1,9 @@
+package Enum;
+
+import java.util.ArrayList;
+
+public class MeioAmbiente {
+    private String MeioAmbiente;
+    private double agua; //litros de agua no meio
+    private ArrayList<SerVivo> seres;
+}
